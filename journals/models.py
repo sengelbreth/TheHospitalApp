61aa220cd882d677567b513d072be315affeb9c6
Sophie <sophie.engel1202@gmail.com>
2020-05-30 22:03:57 +0200

    model for the doctor and the patiant, records

--- journals/models.py
+++ journals/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.urls import reverse 
+
+
+
+class Patient(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this patient')
+    Name = models.CharField(max_length=200)
+    LastName = models.CharField(max_length=200)
+    Adresse = models.CharField(max_length=200)
+    Phone = models.IntegerField(max_length=10)
+    
+    Doctor = models.ForeignKey('Doctor', on_delete=models.SET_NULL, null=True)
+    Records = models.ForeignKey('Records', on_delete=models.SET_NULL, null=True)
+  
+
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.Name
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this patient."""
+        return reverse('patient-detail', args=[str(self.id)])
+
+
+
+class Doctor(models.Model): 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Name = models.CharField(max_length=200)
+    LastName = models.CharField(max_length=200)
+    Field = models.CharField(max_length=200, help_text='field of work for the Doctor')
+    Patient = models.ForeignKey('Patient', on_delete=models.SET_NULL, null=True)
+    Records = models.ForeignKey('Records', on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.Name
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this Doctor."""
+        return reverse('Doctor-detail', args=[str(self.id)])
+
+
+class Records(models.Model): 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+  
+   
+    Doctor = models.ForeignKey('Doctor', on_delete=models.SET_NULL, null=True)
+    Patient = models.ForeignKey('Patient', on_delete=models.SET_NULL, null=True)
+    
