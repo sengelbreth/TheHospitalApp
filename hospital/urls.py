62eff90a457194b9add90049e437500a298d488c
Sophie <sophie.engel1202@gmail.com>
2020-05-30 16:52:23 +0200

    set up of jounrals

--- hospital/urls.py
+++ hospital/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the catalog application 
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
