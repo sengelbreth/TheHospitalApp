19aebd287d2378309370d7a90f72b313d03e6d9a
Sophie <sophie.engel1202@gmail.com>
2020-05-30 16:56:37 +0200

    urls

--- hospital/urls.py
+++ hospital/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,6 +27,15 @@ urlpatterns = [
 # Use include() to add paths from the catalog application 
 
 urlpatterns += [
-    path('catalog/', include('catalog.urls')),
+    path('journals/', include('journals.urls')),
 ]
 
+#redirect the base URL to application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='journals/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
